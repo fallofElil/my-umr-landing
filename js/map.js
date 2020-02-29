5fad51058155bcb4e597a52b2c86f1328b11794b
elil_ <ikiyanov@protonmail.com>
2020-02-29 22:39:26 +0300

    adds js to body

--- js/map.js
+++ js/map.js
@@ -1,16 +1,15 @@
-ymaps.ready(function() {
-    var myMap = new ymaps.Map('map', {
+ymaps.ready(function () {
+    var myMap = new ymaps.Map('idMap', {
         center: [45.034222, 38.912215],
         zoom: 15
     });
 
-    myMap.geoObjects
-        .add(new ymaps.Placemark([45.032871, 38.916306], {
-            balloonContent: 'Развёрнутая инфа',
-            iconCaption: 'Памятник Чернобальцам Кубани'
-        }, {
-            preset: 'islands#greenDotIconWithCaption'
-        }))
+    myMap.geoObjects.add(new ymaps.Placemark([45.032871, 38.916306], {
+        balloonContent: 'Развёрнутая инфа',
+        iconCaption: 'Памятник Чернобальцам Кубани'
+    }, {
+        preset: 'islands#greenDotIconWithCaption'
+    }))
         .add(new ymaps.Placemark([45.030785, 38.911241], {
             balloonContent: 'Памятник чекистам на Платановом бульваре',
             iconCaption: 'Памятник Чекистам'
